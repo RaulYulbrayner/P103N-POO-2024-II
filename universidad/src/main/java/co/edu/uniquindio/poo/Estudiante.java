760e3cc3cb8eac5c574fb6a6bee3ccc07a9d1520
RaulYulbrayner <153127665+RaulYulbrayner@users.noreply.github.com>
2024-08-20 21:35:44 -0500

    update

---
+++ universidad/src/main/java/co/edu/uniquindio/poo/Estudiante.java
@@ -0,0 +1,16 @@
+package co.edu.uniquindio.poo;
+
+public class Estudiante {
+
+    public String nombre;
+    public String apellido;
+    public String numeroIdentificacion;
+    public String correo;
+    public String telefono;
+    public int edad;
+
+
+
+
+    
+}
