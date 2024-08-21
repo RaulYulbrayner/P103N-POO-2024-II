882ac79ea60d50dd18065b8a7b734285701287c5
RaulYulbrayner <153127665+RaulYulbrayner@users.noreply.github.com>
2024-08-21 07:33:52 -0500

    update

---
+++ repaso/src/main/java/co/edu/uniquindio/poo/SumaArreglo.java
@@ -0,0 +1,66 @@
+package co.edu.uniquindio.poo;
+
+import java.util.Scanner;
+
+public class SumaArreglo {
+
+    // Escribe un programa que reciba un arreglo de números enteros y calcule la
+    // suma de todos sus elementos
+
+    /**
+     * Metodo que permite ingresar datos a un arreglo
+     * @param tamanio del arreglo
+     * @return arreglo con los datos ingresados por el usuario
+     */
+    public static int[] ingresarDatos(int tamanio) {
+        Scanner scanner = new Scanner(System.in);
+        int[] arreglo = new int[tamanio];
+        System.out.println("Ingrese los elementos del arreglo:");
+        for (int i = 0; i < tamanio; i++) {
+            System.out.print("Elemento " + (i + 1) + ": ");
+           arreglo[i] = scanner.nextInt();
+        }
+        return arreglo;
+    }
+
+    /**
+     * Metodo que permite imprimir el resultado de la suma del arreglo
+     * @param suma
+     */
+    public static void imprimirSuma(int suma) {
+        System.out.println("La suma de los elementos del arreglo es: " + suma);
+    }
+
+    /**
+     * Metodo que permite sumar todos los elementos de un arreglo
+     * @param arreglo de numeros enteros que se va a sumar
+     * @return la suma de todos los elementos del arreglo
+     */
+    public static int sumarElementos(int[] arreglo) {
+        int suma = 0;
+        for (int i = 0; i < arreglo.length; i++) {
+            suma += arreglo[i];
+        }
+        return suma;
+    }
+
+    /**
+     * Metodo que permite al usuario ingresar el tamaño del arreglo
+     * @return tamaño del arreglo ingresado por el usuario
+     */
+    public static int ingresarTamanio() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el tamaño del arreglo: ");
+        return scanner.nextInt();
+    }
+
+    
+    public static void main(String[] args) {
+        int tamanio = ingresarTamanio();
+        int[] arreglo = ingresarDatos(tamanio);
+        int suma = sumarElementos(arreglo);
+        imprimirSuma(suma);
+    }
+
+    
+}
