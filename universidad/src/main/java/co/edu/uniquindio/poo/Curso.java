80ca2ba2aa155cbeae8af6d6da05b4534aaa8432
RaulYulbrayner <153127665+RaulYulbrayner@users.noreply.github.com>
2024-08-28 08:13:32 -0500

    update

--- universidad/src/main/java/co/edu/uniquindio/poo/Curso.java
+++ universidad/src/main/java/co/edu/uniquindio/poo/Curso.java
@@ -4,12 +4,11 @@ import java.util.Collection;
 import java.util.LinkedList;
 
 public class Curso {
-    
+
     private String nombre;
     private double notaDef;
     private Profesor profesor;
     public Collection<Estudiante> estudiantes;
-
 
     public Curso(String nombre, double notaDef, Profesor profesor) {
         this.nombre = nombre;
@@ -18,59 +17,44 @@ public class Curso {
         estudiantes = new LinkedList<>();
     }
 
-    /*calcularNotaDe(notas)
-    - agregarEstudiante(Estudiante)
-    - verificarEstudiante(double cedula)
-    - eliminarEstudiante(double cedula)*/
-
-
-
-
+    /*
+     * Metodos que deben realizar
+     * - calcularNotaDe(notas)
+     * - agregarEstudiante(Estudiante)
+     * - verificarEstudiante(double cedula)
+     * - eliminarEstudiante(double cedula)
+     */
 
     public String getNombre() {
         return nombre;
     }
 
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
 
     public double getNotaDef() {
         return notaDef;
     }
 
-
     public void setNotaDef(double notaDef) {
         this.notaDef = notaDef;
     }
-
 
     public Profesor getProfesor() {
         return profesor;
     }
 
-
     public void setProfesor(Profesor profesor) {
         this.profesor = profesor;
     }
-
 
     public Collection<Estudiante> getEstudiantes() {
         return estudiantes;
     }
 
-
     public void setEstudiantes(Collection<Estudiante> estudiantes) {
         this.estudiantes = estudiantes;
     }
-
-    
-
-
-    
-
-
 
 }
