88e5998d0e30e593e7fd26ee2e562f60dec561ec
RaulYulbrayner <153127665+RaulYulbrayner@users.noreply.github.com>
2024-10-22 21:27:32 -0500

    update

--- zoologico/src/main/java/co/edu/uniquindio/poo/App.java
+++ zoologico/src/main/java/co/edu/uniquindio/poo/App.java
@@ -6,13 +6,13 @@ package co.edu.uniquindio.poo;
  */
 public class App {
     public static void main(String[] args) {
-        Gato gato = new Gato("Tom", "Gris", 2);
+        Animal gato = new Gato("Tom", "Gris", 2);
         System.out.println(gato.toString());
         gato.comer();
         gato.dormir();
         gato.hacerSonido();
         System.out.println("----------------------");
-        Perro perro = new Perro("Nina", "Cafe", 3);
+        Animal perro = new Perro("Nina", "Cafe", 3);
         System.out.println(perro.toString());
         perro.comer();
         perro.dormir();
